0cd4e66f098d408e06a2a2079c3aeacdb001a228
endsiqueira <endsiqueira@gmail.com>
2022-07-06 22:38:56 -0300

    teste

---
+++ z13/src/main/java/com/example/z13/Z13Application.java
@@ -0,0 +1,13 @@
+package com.example.z13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Z13Application.class, args);
+    }
+
+}
---
+++ z13/src/test/java/com/example/z13/Z13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.z13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
